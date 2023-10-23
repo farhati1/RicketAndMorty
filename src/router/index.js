823844571b007c758481ab3c5950818f1ae86d29
farhati1 <114987105+farhati1@users.noreply.github.com>
2023-10-23 12:19:33 +0200

    j'ai oublié de commit une page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Characters from "../components/Characters.vue";
-import CharactersDetails from "../components/CharactersDetails.vue";
+
 
 
 const router = createRouter({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,23 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js,vue}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#FF6363",
-        secondary: "#E2E2D5",
-      },
-    },
-    fontFamily: {
-      body: ["Nunito"],
-    },
-    container: {
-      center: true,
-      padding: "1rem",
-    },
-    screens: {
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
+export const content = ["./index.html", "./src/**/*.{html,js,vue}"];
+export const theme = {
+  extend: {
+    colors: {
+      primary: "#FF6363",
+      secondary: "#E2E2D5",
     },
   },
-  plugins: [],
+  fontFamily: {
+    body: ["Nunito"],
+  },
+  container: {
+    center: true,
+    padding: "1rem",
+  },
+  screens: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
 };
+export const plugins = [];
